f54e72cfd33c3275da3321a8a6287c1f2519c458
chinchalinchin <chinchalinchin@gmail.com>
2020-12-14 10:43:00 -0500

    update logging components, rename data to cache

--- app/statistics.py
+++ app/statistics.py
@@ -7,128 +7,161 @@ import time
 def retrieve_stock_data(ticker):
     output = utilities.Logger('app.pyfin.retrieve_stock_data')
 
-    url=f'{utilities.QUERY_URL}={ticker}'
-
     buffer_store= os.path.join(utilities.BUFFER_DIR, f'{ticker}.json')
+    # check if price history exists in cache
     if os.path.isfile(buffer_store):
+        output.debug(f'Loading in cached prices...')
         with open(buffer_store, 'r') as infile:
             prices = json.load(infile)
-    else:          
+
+    # retrieve prices from external source
+    else:     
+        output.debug(f'Retrieving prices from AlphaVantage query...')
+        url=f'{utilities.QUERY_URL}={ticker}'     
         prices = requests.get(url).json()
 
+        # check for bad response
         if list(prices.keys())[0] == 'Error Message':
             output.debug(prices['Error Message'])
             return False
+
+        # check for API rate limit 
         while list(prices.keys())[0] == 'Note':
+            output.debug(f'Waiting for AlphaVantage rate limit to refresh...')
             time.sleep(10)
             prices = requests.get(url).json()
 
         prices = prices['Time Series (Daily)']
-        # save prices to buffer for quick access
+
+        # save prices to cache for quick access
         dump_file = os.path.join(utilities.BUFFER_DIR,f'{ticker}.json')
+        output.debug(f'')
         with open(dump_file, 'w') as outfile:
             json.dump(prices, outfile)
 
     return prices
 
 # NOTE: AlphaVantage returns price history fom latest to earliest date.
-def calculate_risk_return(ticker, preloaded_prices=None):
+def calculate_risk_return(ticker, input_prices=None):
     output = utilities.Logger('app.pyfin.calculate_statistics')
-    if preloaded_prices is None:
-        prices = retrieve_stock_data(ticker)
-        if not prices:
-            return False
-    else: 
-        prices = preloaded_prices
 
-    sample = len(prices)
+    # check if results exist in buffer directory
+    buffer_store= os.path.join(utilities.BUFFER_DIR, f'{ticker}_statistics.json')
+    if os.path.isfile(buffer_store):
+        with open(buffer_store, 'r') as infile:
+            results = json.load(infile)
+    else:
+        # check if prices have been passed in as an argument
+        if input_prices is None:
+            prices = retrieve_stock_data(ticker)
+            if not prices:
+                return False
+        else: 
+            prices = input_prices
 
-    # RETURN CALCULATION
-    i = 0 
-    mean_return = 0
-    tomorrows_price = 0
-    for date in prices:
-        todays_price = prices[date]['4. close']
-        if i != 0:
-            daily_return = numpy.log(float(tomorrows_price)/float(todays_price))/utilities.ONE_TRADING_DAY
-            mean_return = mean_return + daily_return/sample 
-        tomorrows_price = prices[date]['4. close']
-        i += 1
-    output.log(f'{ticker} Annualized Return', mean_return)
+        sample = len(prices)
+
+        # calculate mean annual return
+        i = 0 
+        mean_return = 0
+        tomorrows_price = 0
+        for date in prices:
+            todays_price = prices[date]['4. close']
+            if i != 0:
+                daily_return = numpy.log(float(tomorrows_price)/float(todays_price))/utilities.ONE_TRADING_DAY
+                mean_return = mean_return + daily_return/sample 
+            tomorrows_price = prices[date]['4. close']
+            i += 1
+        output.log(f'{ticker} \u03BC', mean_return)
 
 
-    # VOLATILITY CALCULATION
-    i = 0
-    mean_mod_return = mean_return*numpy.sqrt(utilities.ONE_TRADING_DAY)
-    variance = 0
-    tomorrows_price = 0
-    for date in prices:
-        todays_price = prices[date]['4. close']
-        if i != 0:
-            current_mod_return= numpy.log(float(tomorrows_price)/float(todays_price))/numpy.sqrt(utilities.ONE_TRADING_DAY) 
-            variance = variance + (current_mod_return - mean_mod_return)**2/(sample - 1)
-        tomorrows_price = prices[date]['4. close']
-        i += 1
-    volatility = numpy.sqrt(variance)
-    results = {
-        'annual_return': mean_return,
-        'annual_volatility': volatility
-    }
+        # calculate sample annual volatility
+        i = 0
+        mean_mod_return = mean_return*numpy.sqrt(utilities.ONE_TRADING_DAY)
+        variance = 0
+        tomorrows_price = 0
+        for date in prices:
+            todays_price = prices[date]['4. close']
+            if i != 0:
+                current_mod_return= numpy.log(float(tomorrows_price)/float(todays_price))/numpy.sqrt(utilities.ONE_TRADING_DAY) 
+                variance = variance + (current_mod_return - mean_mod_return)**2/(sample - 1)
+            tomorrows_price = prices[date]['4. close']
+            i += 1
+        volatility = numpy.sqrt(variance)
+        results = {
+            'annual_return': mean_return,
+            'annual_volatility': volatility
+        }
 
-    dump_file = os.path.join(utilities.BUFFER_DIR,f'{ticker}_statistics.json')
-    with open(dump_file, 'w') as outfile:
-      json.dump(results, outfile)
-    output.log(f'{ticker} Annualized Volatility', volatility)
+        # Store results in buffer for quick access
+        dump_file = os.path.join(utilities.BUFFER_DIR,f'{ticker}_statistics.json')
+        with open(dump_file, 'w') as outfile:
+            json.dump(results, outfile)
+        output.log(f'\u03C3_{ticker}', volatility)
     
     return results
 
 def calculate_correlation(ticker_1, ticker_2):
-    prices_1 = retrieve_stock_data(ticker_1)
-    prices_2 = retrieve_stock_data(ticker_2)
-    if not (prices_1 and prices_2):
-        return False 
-    
-    stats_1 = calculate_risk_return(ticker_1, prices_1)
-    stats_2 = calculate_risk_return(ticker_2, prices_2)
+    buffer_store_1= os.path.join(utilities.BUFFER_DIR, f'{ticker_1}_{ticker_2}_correlation.json')
+    buffer_store_2= os.path.join(utilities.BUFFER_DIR, f'{ticker_2}_{ticker_1}_correlation.json')
 
-    mod_mean_1 = stats_1['annual_return']*numpy.sqrt(utilities.ONE_TRADING_DAY)
-    mod_mean_2 = stats_2['annual_return']*numpy.sqrt(utilities.ONE_TRADING_DAY)
+    # check if results exist in cache location 1
+    if os.path.isfile(buffer_store_1):
+        with open(buffer_store_1, 'r') as infile:
+            results = json.load(infile)
+            correlattion = results['correlation']
 
-    # CORRELATION CALCULATION
-    i = 0 
-    covariance = 0
-    tomorrows_price_1 = 0
-    tomorrows_price_2 = 0
-    if len(prices_1) == len(prices_2) or len(prices_1) < len(prices_2):
-        sample_prices = prices_1
+    # check if results exist in cache location 2
+    elif os.path.isfile(buffer_store_2):
+        with open(buffer_store_2, 'r') as infile:
+            results = json.load(infile)
+            correlattion = results['correlation']
+
+    # calculate results from sample
     else:
-        sample_prices = prices_2
-  
-    sample = len(sample_prices)
-    for date in sample_prices:
-            todays_price_1 = prices_1[date]['4. close']
-            todays_price_2 = prices_2[date]['4. close']
-            if i != 0:
-                current_mod_return_1= numpy.log(float(tomorrows_price_1)/float(todays_price_1))/numpy.sqrt(utilities.ONE_TRADING_DAY) 
-                current_mod_return_2= numpy.log(float(tomorrows_price_2)/float(todays_price_2))/numpy.sqrt(utilities.ONE_TRADING_DAY) 
+        prices_1 = retrieve_stock_data(ticker_1)
+        prices_2 = retrieve_stock_data(ticker_2)
+        if not (prices_1 and prices_2):
+            return False 
+        
+        stats_1 = calculate_risk_return(ticker_1, prices_1)
+        stats_2 = calculate_risk_return(ticker_2, prices_2)
 
-                covariance = covariance + (current_mod_return_1 - mod_mean_1)*(current_mod_return_2 - mod_mean_2)/(sample - 1)
-            tomorrows_price_1 = prices_1[date]['4. close']
-            tomorrows_price_2 = prices_2[date]['4. close']
-            i += 1
+        mod_mean_1 = stats_1['annual_return']*numpy.sqrt(utilities.ONE_TRADING_DAY)
+        mod_mean_2 = stats_2['annual_return']*numpy.sqrt(utilities.ONE_TRADING_DAY)
 
-    correlation = covariance/ (stats_1['annual_volatility']*stats_2['annual_volatility'])
+        # calculate correlation
+        i = 0 
+        covariance = 0
+        tomorrows_price_1 = 0
+        tomorrows_price_2 = 0
+        if len(prices_1) == len(prices_2) or len(prices_1) < len(prices_2):
+            sample_prices = prices_1
+        else:
+            sample_prices = prices_2
+    
+        sample = len(sample_prices)
+        for date in sample_prices:
+                todays_price_1 = prices_1[date]['4. close']
+                todays_price_2 = prices_2[date]['4. close']
+                if i != 0:
+                    current_mod_return_1= numpy.log(float(tomorrows_price_1)/float(todays_price_1))/numpy.sqrt(utilities.ONE_TRADING_DAY) 
+                    current_mod_return_2= numpy.log(float(tomorrows_price_2)/float(todays_price_2))/numpy.sqrt(utilities.ONE_TRADING_DAY) 
+                    covariance = covariance + (current_mod_return_1 - mod_mean_1)*(current_mod_return_2 - mod_mean_2)/(sample - 1)
+                tomorrows_price_1 = prices_1[date]['4. close']
+                tomorrows_price_2 = prices_2[date]['4. close']
+                i += 1
 
-    output.log(f'({ticker_1}, {ticker_2}) Annualized Covariance', covariance)
-    output.log(f'({ticker_1}, {ticker_2}) Annualized Correlation', correlation)
+        correlation = covariance/ (stats_1['annual_volatility']*stats_2['annual_volatility'])
 
-    results = {
-        'correlation': correlation
-    }
+        output.log(f'covar_({ticker_1}, {ticker_2})', covariance)
+        output.log(f'\u03A1_({ticker_1}, {ticker_2})', correlation)
 
-    dump_file = os.path.join(utilities.BUFFER_DIR,f'{ticker_1}_{ticker_2}_correlation.json')
-    with open(dump_file, 'w') as outfile:
-      json.dump(results, outfile)
+        results = { 'correlation': correlation }
+
+        dump_file = os.path.join(utilities.BUFFER_DIR,f'{ticker_1}_{ticker_2}_correlation.json')
+        with open(dump_file, 'w') as outfile:
+            json.dump(results, outfile)
 
     return correlation
 
@@ -140,18 +173,31 @@ if __name__ == "__main__":
     for f in filelist:
         os.remove(os.path.join(utilities.BUFFER_DIR, f))
 
-    args = sys.argv[1:]
-    # calculate_correlation(args[1], args[2])
-    if(len(args) > 1):
-        for i in range(len(args)):
-            for j in range(i+1, len(args)):
-                output.title_line(f'({args[i]}, {args[j]}) Calculations')
-                flag = calculate_correlation(args[i], args[j]) 
-                if not flag:
-                    output.debug('Error Encountered While Calculating. Try Again.')
-    elif len(args) == 1:
-        flag = calculate_risk_return(args[0])
-        if not flag:
-            output.debug('Error Encountered While Calculating. Try Again')
-    elif len(args == 0):
-        output.debug('No Input Supplied. Try Again.')
+    opt = sys.argsv[1]
+    if opt == "-h":
+        output.help()
+    
+    else:
+        args = sys.argv[2:]
+        if opt:
+            pass
+
+
+
+
+
+
+
+        if(len(args) > 1):
+            for i in range(len(args)):
+                for j in range(i+1, len(args)):
+                    output.title_line(f'({args[i]}, {args[j]}) Calculations')
+                    flag = calculate_correlation(args[i], args[j]) 
+                    if not flag:
+                        output.debug('Error Encountered While Calculating. Try Again.')
+        elif len(args) == 1:
+            flag = calculate_risk_return(args[0])
+            if not flag:
+                output.debug('Error Encountered While Calculating. Try Again')
+        elif len(args == 0):
+            output.debug('No Input Supplied. Try Again.')
--- app/utilities.py
+++ app/utilities.py
@@ -1,12 +1,16 @@
 import datetime, os, dotenv
 
 APP_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BUFFER_DIR = os.path.join(APP_DIR, 'data')
+BUFFER_DIR = os.path.join(APP_DIR, 'cache')
 ENV = dotenv.load_dotenv(os.path.join(APP_DIR,'.env'))
 QUERY_URL = os.getenv('AV_QUERY_URL')
 ONE_TRADING_DAY=(1/252)
 DEBUG=True
 SEPARATER="-------------------------"
+FUNC_DICT={
+    "correlation":"-c",
+    "help": "-h"
+}
 
 class Logger():
 
@@ -26,3 +30,12 @@ class Logger():
 
     def title_line(self, title):
         print(SEPARATER, title, SEPARATER) 
+
+    def option(self, opt, explanation):
+        print("      -", opt, " = ", explanation)
+
+    def help(self):
+        print('command -OPTION [tickers]')
+        title_line('OPTIONS')
+        option(FUNC_DICT['correlation'], 'Calculate pair-wise correlation (\u03A1) for the list of tickers supplied.')
+        option(FUNC_DICT['help'], 'Print this help message')
