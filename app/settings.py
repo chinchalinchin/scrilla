d24a12545c90f163b602710422462b019025cad2
chinchalinchin <chinchalinchin@gmail.com>
2020-12-24 08:00:19 -0500

    add env vars for moving average calculations

--- app/settings.py
+++ app/settings.py
@@ -16,16 +16,35 @@ AV_QUERY_URL = os.getenv('AV_QUERY_URL')
 
 PRICE_MANAGER = os.getenv('PRICE_MANAGER')
 
-try:
-    FRONTIER_STEPS = int(os.getenv('FRONTIER_STEPS'))
-except:
-    output.debug('Failed to parse FRONTIER_STEPS from .env File. Please Ensure FRONTIER_STEPS is set to an integer value.')
-    FRONTIER_STEPS = 5
-
 DEBUG= True if os.getenv('DEBUG').lower() == 'true' else False
 
 INVESTMENT_MODE = True if os.getenv('INVESTMENT_MODE').lower() == 'true' else False
 
+try:
+    FRONTIER_STEPS = int(os.getenv('FRONTIER_STEPS'))
+
+except:
+    output.debug('Failed to parse FRONTIER_STEPS from .env File. Please Ensure FRONTIER_STEPS is set to an integer value.')
+    FRONTIER_STEPS = 5
+
+try:
+    MA_1 = int(os.getenv('MA_1'))
+except: 
+    output.debug('Failed to parse MA_1 from .env File. Please Ensure MA_1 is set to an integer value.')
+    MA_1 = 20
+
+try:
+    MA_2 = int(os.getenv('MA_2'))
+except: 
+    output.debug('Failed to parse MA_2 from .env File. Please Ensure MA_2 is set to an integer value.')
+    MA_2 = 60
+
+try:
+    MA_3 = int(os.getenv('MA_3'))
+except: 
+    output.debug('Failed to parse MA_3 from .env File. Please Ensure MA_3 is set to an integer value.')
+    MA_1 = 100
+      
 ONE_TRADING_DAY=(1/252)
 
 SEPARATER="-"
@@ -34,7 +53,7 @@ LINE_LENGTH=100
 
 INDENT=10
 
-HELP_MSG="A financial application written in python to determine optimal portfolio allocations subject to various constraints and calculate various statistics concerning a given portfolio allocation."
+HELP_MSG="A financial application written in python to determine optimal portfolio allocations subject to various constraints and calculate fundamental statistics concerning a given portfolio allocation. Note: all calculations are based on an equity's closing price for the past 100 trading days. "
 
 SYNTAX="command -OPTION [tickers] (additional input)"
 
@@ -67,5 +86,6 @@ EXAMPLES = {
     'python ./main.py -cor GLD SPY SLV UUP TLT EWA': 'Calculate the correlation matrix for the portfolio composed of (GLD, SPY, SLV, UUP, TLT, EWA',
     'python ./main.py -min U TSLA SPCE': 'Find the portfolio allocation that minimizes the overall variance of the portfolio composed of (U, TSLA, SPCE). ',
     'python ./main.py -opt ALLY FB PFE SNE BX 0.83': 'Optimize the portfolio consisting of (ALLY, FB, PFE, SNE, BX) subject to the constraint their mean annual return equal 83%. Note the constrained return must reside within the feasible region of returns, i.e. the constrained return must be less than the maximum possible return.',  
-    'python ./main.py -ef QS DIS RUN': 'Calculate a five point sample of the efficient portfolio (risk, return) frontier for the portfolio composed of (QS, DIS, RUN). The number of points generated in the sample can be altered through the FRONTIER_STEPS environment variable.'
+    'python ./main.py -ef QS DIS RUN': 'Calculate a five point sample of the efficient portfolio (risk, return) frontier for the portfolio composed of (QS, DIS, RUN). The number of points generated in the sample can be altered through the FRONTIER_STEPS environment variable.',
+    'python ./main.py -pef QQQ SPY DIA': 'Generate a graphical display of the (QQQ, SPY, DIA) portolio\'s efficient (risk, return) frontier. The number of points generated in the sample can be altered through the FRONTIER_STEPS environment variable.'
 }
