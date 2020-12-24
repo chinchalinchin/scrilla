34a4434326ee75fe001ff7da2c3920929797e959
chinchalinchin <chinchalinchin@gmail.com>
2020-12-24 08:02:55 -0500

    correct typos in readme

--- app/settings.py
+++ app/settings.py
@@ -44,7 +44,7 @@ try:
 except: 
     output.debug('Failed to parse MA_3 from .env File. Please Ensure MA_3 is set to an integer value.')
     MA_1 = 100
-      
+
 ONE_TRADING_DAY=(1/252)
 
 SEPARATER="-"
@@ -87,5 +87,5 @@ EXAMPLES = {
     'python ./main.py -min U TSLA SPCE': 'Find the portfolio allocation that minimizes the overall variance of the portfolio composed of (U, TSLA, SPCE). ',
     'python ./main.py -opt ALLY FB PFE SNE BX 0.83': 'Optimize the portfolio consisting of (ALLY, FB, PFE, SNE, BX) subject to the constraint their mean annual return equal 83%. Note the constrained return must reside within the feasible region of returns, i.e. the constrained return must be less than the maximum possible return.',  
     'python ./main.py -ef QS DIS RUN': 'Calculate a five point sample of the efficient portfolio (risk, return) frontier for the portfolio composed of (QS, DIS, RUN). The number of points generated in the sample can be altered through the FRONTIER_STEPS environment variable.',
-    'python ./main.py -pef QQQ SPY DIA': 'Generate a graphical display of the (QQQ, SPY, DIA) portolio\'s efficient (risk, return) frontier. The number of points generated in the sample can be altered through the FRONTIER_STEPS environment variable.'
+    'python ./main.py -pef QQQ SPY DIA': 'Generate a graphical display of the (QQQ, SPY, DIA) portolio\'s efficient (risk, return) frontier. The number of points generated in the sample can be altered through the FRONTIER_STEPS environment variable. Note, if the graphical display does not show up, you may need to configure matplotlib\'s backend to be compatible with your OS.'
 }
