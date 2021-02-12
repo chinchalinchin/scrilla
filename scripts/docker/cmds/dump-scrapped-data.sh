a39cea6454ac9e02cd8c995807b17e7365786eba
chinchalinchin <chinchalinchin@gmail.com>
2021-02-12 10:01:29 -0500

    update dump script to copy dumpfile to local file system after generation

--- scripts/docker/cmds/dump-scrapped-data.sh
+++ scripts/docker/cmds/dump-scrapped-data.sh
@@ -3,12 +3,30 @@ SCRIPT_NAME='dump-scrapped-data'
 nl=$'\n'
 tab="     "
 ind="   "
-SCRIPT_DES="" 
+SCRIPT_DES="This script will dump the containerized database into a local zip file \
+defined by the path passed as the first argument.${nl}${ind}For example,${nl}${nl}${tab}\
+${tab}bash $SCRIPT_NAME.sh $(pwd)${nl}${nl}${ind}will dump the database into a file\
+within your present working directory." 
 
 source "$SCRIPT_DIR/../../util/logging.sh"
 
+if [ "$1" == "--help" ] || [ "$1" == "--h" ] || [ "$1" == "-help" ] || [ "$1" == "-h" ]
+then
+    help "$SCRIPT_DES" $SCRIPT_NAME
+else
+    if [ ! $# -eq 0 ]
+    then
+        DUMP_PATH="/home/pynance.gz"
+        DUMP_CMD="PGPASSWORD=$POTSGRES_PASSWORD pg_dump --username=$POSTGRES_USER --dbname pynance | gzip > $DUMP_PATH"
+        CONTAINER_ID="$(docker container ps --filter name=datasource --quiet)"
 
-# TODO: after scrapping, dump database into sql file
-# TODO: copy file from container virtual file system to local file system
+        log "Executing dump command within Container ID# $CONTAINER_ID" $SCRIPT_NAME
+        docker exec $CONTAINER_ID bash -c "$DUMP_CMD"
 
-# docker cp CONTAINER_PATH LOCAL_PATH
+        log "Copying dump file to local filesytem at $1" $SCRIPT_NAME
+        docker cp $CONTAINER_ID:$DUMP_PATH $1
+    else
+        log "No argument provided, unable to execute script. Please see help message below and try again." $SCRIPT_NAME
+        help "$SCRIPT_DES" $SCRIPT_NAME
+    fi
+fi
