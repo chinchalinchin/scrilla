72aff662c270b71c96d6c06a0337327805d2ccf3
chinchalinchin <chinchalinchin@gmail.com>
2021-02-11 20:48:01 -0500

    tinker with scrapping algorithm

--- server/pynance_api/scrap.py
+++ server/pynance_api/scrap.py
@@ -18,27 +18,26 @@ import app.settings as app_settings
 import util.helper as helper
 import util.logger as logger
 
-output = logger.Logger("server.pynance_api.scrap")
+output = logger.Logger("server.pynance_api.scrap", settings.LOG_LEVEL)
 
 # Must be done after /static/ is initialized!
 def scrap():
     today = datetime.date.today()
     asset_type = app_settings.ASSET_EQUITY
-    symbols = list(services.get_static_data(app_settings.ASSET_EQUITY))
-
+    symbols = list(services.get_static_data(asset_type))
 
     for symbol in symbols:
-        logger.debug(f'Saving {symbol} to EquityTicker table in database')
-        new_ticker_entry = EquityTicker.objects.get_or_create(ticker=symbol)
-
-        logger.debug(f'Retrieving price history for {symbol}...')
-        price_history = services.get_daily_price_history(symbol, full=True)
+        output.debug(f'Saving {symbol} to EquityTicker table in database')
+        new_ticker_entry = EquityTicker.objects.get_or_create(ticker=symbol)[0]
+        
+        output.debug(f'Retrieving price history for {symbol}...')
+        price_history = services.query_service_for_daily_price_history(symbol, full=True)
         
         for date in price_history:
-            todays_price = services.parse_price_from_date(price_history=price_history, date=date, asset_type=asset_type)
+            todays_price = services.parse_price_from_date(prices=price_history, date=date, asset_type=asset_type)
             todays_date = helper.parse_date_string(date)
                 
-            logger.debug(f'Saving {symbol} closing price of {todays_price} on {date} to EquityMarket table in database.')
+            output.debug(f'Saving {symbol} closing price of {todays_price} on {date} to EquityMarket table in database.')
             new_market_entry = EquityMarket.objects.get_or_create(ticker=new_ticker_entry, date=date, closing_price=todays_price)
     
 
