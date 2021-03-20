9feaa97a9ba4f6dc827531ed02fdec8fc3e43c69
chinchalinchin <chinchalinchin@gmail.com>
2021-03-20 14:03:47 -0400

    create script to dump only the database schema

---
+++ scripts/docker/cmds/dump-schema.sh
@@ -0,0 +1,37 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT_NAME='dump-data'
+nl=$'\n'
+tab="     "
+ind="   "
+SCRIPT_DES="This script will dump a containerized database table specifed by the first argument \
+into a local zip file ${nl}${ind} in the \e[3m/data/sql/\e[0m directory. For example,${nl}${nl}\
+${tab}${tab}\e[2mbash $SCRIPT_NAME.sh data_equitymarket\e[0m${nl}${nl}${ind} will dump the database table \
+\e[1mdata_equitymarket\e[0m into the file \e[3m/data/sql/data_equitymarket.gz\e[0m." 
+
+source "$SCRIPT_DIR/../../util/logging.sh"
+
+if [ "$1" == "--help" ] || [ "$1" == "--h" ] || [ "$1" == "-help" ] || [ "$1" == "-h" ]
+then
+    help "$SCRIPT_DES" $SCRIPT_NAME
+else
+    if [ ! $# -eq 0 ]
+    then        
+        source "$SCRIPT_DIR/../../util/env-vars.sh" container
+            # reset SCRIPT_DIR since sourcing overwrites it.
+        SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+        DUMP_PATH="/home/$POSTGRES_DB-$1_schema.gz"
+        LOCAL_PATH="$SCRIPT_DIR/../../../data/sql/"
+        DUMP_CMD="PGPASSWORD=$POSTGRES_PASSWORD pg_dump --schema-only --username=$POSTGRES_USER --dbname=$POSTGRES_DB| gzip > $DUMP_PATH"
+        CONTAINER_ID="$(docker container ps --filter name=$POSTGRES_HOST --quiet)"
+
+        log "Executing dump command within Container ID # $CONTAINER_ID" $SCRIPT_NAME
+        docker exec $CONTAINER_ID bash -c "$DUMP_CMD"
+
+        log "Copying container dump file to local filesytem at $LOCAL_PATH/$POSTGRES_DB-$1.gz" $SCRIPT_NAME
+            # the cp command may or may not need quotes...
+        docker cp "$CONTAINER_ID:$DUMP_PATH" "$LOCAL_PATH"
+    else
+        log "No argument provided, unable to execute script. Please see help message below and try again." $SCRIPT_NAME
+        help "$SCRIPT_DES" $SCRIPT_NAME
+    fi
+fi
