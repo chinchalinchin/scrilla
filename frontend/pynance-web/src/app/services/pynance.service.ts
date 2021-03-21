58c2b9d1a03bca269844eef241b083f10e72236d
chinchalinchin <chinchalinchin@gmail.com>
2021-03-21 11:07:18 -0400

    format backend query in pynance service, introduce rxjs operators to catch errors and log respons

--- frontend/pynance-web/src/app/services/pynance.service.ts
+++ frontend/pynance-web/src/app/services/pynance.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Portfolio } from '../models/portfolio';
 import { LogService } from './log.service';
+import {environment} from '../../environments/environment'
 
 const ENDPOINTS ={
   optimize:{
-    endpoint:'/api/optimize',
+    endpoint:'api/optimize',
     parameters:{
       tickers: 'tickers',
       startDate: 'start',
@@ -20,17 +23,50 @@ const ENDPOINTS ={
   providedIn: 'root'
 })
 export class PynanceService {
-  private location : string = "app.services.pynance"
+  private location : string = "app.services.pynance";
 
   constructor(private logs: LogService,
               private http: HttpClient) { }
 
 
-  public optimize(): void{
-    //TODO
+  public formatQueryTickers(tickers: string[]) : string{
+    let query: string = "";
+    let first: boolean = true;
+    for(let ticker of tickers){
+      if (first){
+        query = query + `${ENDPOINTS.optimize.parameters.tickers}=${ticker}`;
+        first = false;
+      }
+      else{
+        query = query + `&${ENDPOINTS.optimize.parameters.tickers}=${ticker}`;
+      }
+    }
+    return query;
   }
 
-  /**
+  public optimize(tickers: string[], endDate : string = null, startDate : string = null, 
+                  targetReturn : number = null, investment : number = null): Observable<Portfolio>{
+    let baseUrl: string = `${environment.backendUrl}/${ENDPOINTS.optimize.endpoint}?`;
+    let query: string = this.formatQueryTickers(tickers)
+
+    if (endDate){ query = query + `&${ENDPOINTS.optimize.parameters.endDate}=${endDate}`; }
+    if(startDate){ query = query + `&${ENDPOINTS.optimize.parameters.startDate}=${startDate}`; }
+    if(targetReturn){ query = query + `&${ENDPOINTS.optimize.parameters.targetReturn}=${targetReturn}`; }
+    if(investment){ query = query + `&${ENDPOINTS.optimize.parameters.investment}=${investment}`}
+
+    let queryUrl = baseUrl + "?" + query
+
+    this.logs.log(`Querying backend for ${queryUrl}`, this.location)
+
+    return this.http.get<Portfolio>(queryUrl)
+      .pipe( 
+        tap((response) => this.logs.log(`Received response ${response}`, this.location)),
+        catchError(this.handleError<Portfolio>('optimize', null))
+    );
+  }
+
+
+/**
  * Handle Http operation that failed.
  * Let the app continue.
  * @param operation - name of the operation that failed
@@ -38,9 +74,6 @@ export class PynanceService {
  */
 private handleError<T>(operation = 'operation', result?: T) {
   return (error: any): Observable<T> => {
-
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
 
     // TODO: better job of transforming error for user consumption
     this.logs.log(`${operation} failed: ${error.message}`, this.location);
