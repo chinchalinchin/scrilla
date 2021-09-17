c03f9a63e1743d9aad2eddb1eb12ec3a55e0969e
Grant Moore <chinchalinchin@gmail.com>
2021-09-17 14:14:16 -0400

    add licensing terms

--- src/scrilla/gui/widgets.py
+++ src/scrilla/gui/widgets.py
@@ -1,3 +1,18 @@
+# This file is part of scrilla: https://github.com/chinchalinchin/scrilla.
+
+# scrilla is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3
+# as published by the Free Software Foundation.
+
+# scrilla is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with scrilla.  If not, see <https://www.gnu.org/licenses/>
+# or <https://github.com/chinchalinchin/scrilla/blob/develop/main/LICENSE>.
+
 from PyQt5 import QtGui, QtCore, QtWidgets
 
 def get_title_font():
