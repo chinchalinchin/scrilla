3ab312dc796db6708ea9f1485539be5021550f49
Grant Moore <chinchalinchin@gmail.com>
2021-09-20 10:48:56 -0400

    updating docstrings

--- src/scrilla/analysis/models/geometric/probability.py
+++ src/scrilla/analysis/models/geometric/probability.py
@@ -46,35 +46,34 @@ def percentile(S0, vol, ret, expiry, percentile, div=0):
 
 def conditional_expected_value(S0, vol, ret, expiry, conditional_value,  greater = False, div=0,):
     """
-    Description
-    -----------
-    Calculates the conditional expected value of an equity as described the Black Scholes model.\n \n
+    Calculates the conditional expected value of an equity, assuming the price process follows Geometric Brownian Motion, i.e. the price distribution is lognormal.\n\n
     
-    Note: Returns either, \n
-        if greater: \n
-            E( St | St > condtional_value) \n
-        if not greater:
-            E( St | St < conditional_value) \n \n
+    Note: Returns either\n
+        >>> if greater:\n
+            >>> E( St | St > condtional_value)\n
+        >>> if not greater:\n
+            >>> E( St | St < conditional_value)\n\n
     
     Parameters
     ----------
-    1. S0 : float \n
-        Initial equity price. \n
-    2. vol : float \n
-        Annualized volatility. \n
-    3. ret: float \n
-        Annualized return. \n
-    4. expiry : float \n 
-        Time in years. \n
-    5. conditional_value: float \n
-        The value of the equity conditioned on at time 'expiry' \n
-    6. greater : boolean \n
-        Defaults to `False`. Determines the direction of the inequality as the condition for the expectation. See Description for more information. \n
-    7. div : float \n
-        Defaults to `0`. Annualized dividend yield. 
+    1. **S0** : ``float``\n
+        Initial equity price.\n
+    2. **vol** : ``float``\n
+        Annualized volatility.\n
+    3. ***ret**: ``float``\n
+        Annualized return.\n
+    4. **expiry** : ``float``\n 
+        Time in years.\n
+    5. **conditional_value**: ``float``\n
+        The value of the equity conditioned on at time ``expiry``\n
+    6. **greater** : ``boolean``\n
+        Defaults to `False`. Determines the direction of the inequality as the condition for the expectation. See Description for more information.\n
+    7. **div**: ``float``\n
+        Defaults to `0`. Annualized dividend yield.\n\n
+
     Notes
     -----
-    1. Important! This is not the risk-neutral conditional expected value; it is the conditional expected value as determined by the stochastic process describing the equity's actual probability distribution.
+    1. Important! This is not the risk-neutral conditional expected value; it is the conditional expected value as determined by the stochastic process describing the equity's actual probability distribution.\n\n
     """
     forward_value = S0*exp( (ret - div)*expiry)
     if greater:
