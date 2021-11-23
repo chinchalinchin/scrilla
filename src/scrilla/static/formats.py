9e35008c479210946d2249b17709090d0bdb044b
Grant Moore <chinchalinchin@gmail.com>
2021-11-23 10:43:51 -0500

    clean branch

--- src/scrilla/static/formats.py
+++
@@ -1,9 +0,0 @@
-formats = {
-    'separator': '-',
-    'TAB': '     ',
-    'LINE_LENGTH': 100,
-    'BAR_WIDTH': 0.10,
-    'INDENT': 10,
-    'RISK_FREE_TITLE': "{} US Treasury",
-    'BINS': 20
-}
